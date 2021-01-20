6e0d18879610d3ba5d3d458d00004c4598d3a90b
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-01-19 20:34:02 -0600

    Create movieScraper.py

---
+++ movieScraper.py
@@ -0,0 +1,17 @@
+import bs4, requests
+
+url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+
+def scrapeMovies(url):
+    '''Scrape IMBDB Top 250 Movies'''
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.text,'lxml')
+
+    tableData = [i.get_text() for i in soup.find_all('td', class_='titleColumn')]
+    
+    with open('file.txt','w') as f:
+        for i in tableData:
+            f.write(str(i.strip('/n')))
+
+
+scrapeMovies(url)
